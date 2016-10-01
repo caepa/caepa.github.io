f070ee96fa430899fb61d484f69b6c6f28b960ea
champa720 <champa720@gmail.com>
2016-10-01 17:56:07 -0600

    event_page_alt schedule

--- js/caepa.materialize.js
+++ js/caepa.materialize.js
@@ -1,55 +1,55 @@
 $( document ).ready(function() {
   'use strict';
-  
+
   // reposition page-content next to table of contents
   var toc = $('#rendezvous #toc');
   // reposition page-content next to table of contents
   $('#rendezvous #page-content').css({ marginTop: -toc.outerHeight()+-20 });
 
 
-    /*  Components */
-    // Badges
-    // Buttons
-    // Breadcrumbs
-    // Cards
-    // Chips
-    // Collections
-    // Footer
-    // Forms
-    $('select').material_select();
-    // Icons
-    // Navbar
-    $('.button-collapse').sideNav({
-      menuWidth: 300,
-      edge: 'left',
-      closeOnClick: false
-    });
-    // Pagination
-    // Preloader
-
-    /* JavaScript */
-    // Collapsible
-    $('.collapsible').collapsible();
-    // Dialogs
-    $('.tooltipped').tooltip({
-      delay: 25
-    });
-    // Dropdown
-    // Media
-    // Models
-    $('.modal-trigger').leanModal();
-    // Parallax
-    $('.parallax').parallax();
-    // Pushpin
-    // Scrollfire
-    // Scrollspy
-    // Sidenav
-    // Tabs
-    $('#tabs').tabs(); // make table-of-contents act like tabs
-    // Transitions
-    // Waves
-
+  /*  Components */
+  // Badges
+  // Buttons
+  // Breadcrumbs
+  // Cards
+  // Chips
+  // Collections
+  // Footer
+  // Forms
+  $('select').material_select();
+  // Icons
+  // Navbar
+  $('.button-collapse').sideNav({
+    menuWidth: 300,
+    edge: 'left',
+    closeOnClick: false
   });
+  // Pagination
+  // Preloader
+
+  /* JavaScript */
+  // Collapsible
+  $('.collapsible').collapsible();
+  // Dialogs
+  $('.tooltipped').tooltip({
+    delay: 25
+  });
+  // Dropdown
+  // Media
+  // Models
+  $('.modal-trigger').leanModal();
+  // Parallax
+  $('.parallax').parallax();
+  // Pushpin
+  // Scrollfire
+  // Scrollspy
+  // Sidenav
+  // Tabs
+  $('#tabs').tabs(); // make table-of-contents act like tabs
+  // Transitions
+  // Waves
+
+});
 
 $(window).on("load resize", function () {
   var toc = $('#rendezvous #toc');
@@ -64,6 +64,10 @@ $(window).on("load resize", function () {
     // reposition page-content next to table of contents
     $('#rendezvous #page-content').css({ marginTop: -toc.outerHeight()+38 });
   }
+  // google charts
+  google.charts.load('current', {'packages':['timeline']});
+  google.charts.setOnLoadCallback(dayOne);  
+  google.charts.setOnLoadCallback(dayTwo);
 });
 
 /* use this to create a dynamic back button
