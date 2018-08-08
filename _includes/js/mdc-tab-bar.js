daa37370a2d34db000f9ad65199fe9da5ec97f42
Jason Kilpatrick <champa720@gmail.com>
2018-08-07 21:32:31 -0600

    IE fixes

--- _includes/js/mdc-tab-bar.js
+++ _includes/js/mdc-tab-bar.js
@@ -1,25 +1,31 @@
-var dynamicTabBar = window.dynamicTabBar = new mdc.tabs.MDCTabBar(document.querySelector('.mdc-tab-bar'));
-var panels = document.querySelector('.panels');
+function initTabs(el) {
 
-dynamicTabBar.tabs.forEach(function(tab) {
-  tab.preventDefaultOnClick = true;
-});
+  var tabs = window.dynamicTabBar = new mdc.tabs.MDCTabBar(document.querySelector('#' + el));
+  var panels = document.querySelector('[for=' + el + ']');
 
-function updatePanel(index) {
-  var activePanel = panels.querySelector('.panel.active');
-  if (activePanel) {
-    activePanel.classList.remove('active');
-    activePanel.setAttribute('aria-hidden', 'true');
-  }
-  var newActivePanel = panels.querySelector('.panel:nth-child(' + (index + 1) + ')');
-  if (newActivePanel) {
-    newActivePanel.classList.add('active');
-    newActivePanel.setAttribute('aria-hidden', 'false');
-  }
-};
+  dynamicTabBar.tabs.forEach(function(tab) {
+    tab.preventDefaultOnClick = true;
+  });
 
-dynamicTabBar.listen('MDCTabBar:change', function ({detail: tabs}) {
-  var nthChildIndex = tabs.activeTabIndex;
+  function updatePanel(index) {
+    var activePanel = panels.querySelector('.panel.active');
+    if (activePanel) {
+      activePanel.classList.remove('active');
+      activePanel.setAttribute('aria-hidden', 'true');
+    }
+    var newActivePanel = panels.querySelector('.panel:nth-child(' + (index + 1) + ')');
+    if (newActivePanel) {
+      newActivePanel.classList.add('active');
+      newActivePanel.setAttribute('aria-hidden', 'false');
+    }
+  };
 
-  updatePanel(nthChildIndex);
-});
+  dynamicTabBar.listen('MDCTabBar:change', function (t) {
+    var tabs = t.detail;
+    var nthChildIndex = tabs.activeTabIndex;
+
+    updatePanel(nthChildIndex);
+  });
+}
+
+initTabs('home-tab-bar');
