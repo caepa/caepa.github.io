2c4248a0d31c37f6d255614e905318d2767b90c7
Jason Kilpatrick <champa720@gmail.com>
2018-08-08 00:18:27 -0600

    Update mdc-top-app-bar

--- _includes/js/mdc-auto-init.js
+++ _includes/js/mdc-auto-init.js
@@ -1,6 +1,9 @@
 window.mdc.autoInit();
 
-var buttons = document.querySelectorAll('.mdc-button, .mdc-fab, .mdc-image-list__image-aspect-container');
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = mdc.topAppBar.MDCTopAppBar.attachTo(topAppBarElement);
+
+const buttons = document.querySelectorAll('.mdc-button, .mdc-fab, .mdc-image-list__image-aspect-container');
 for (var i = 0, button; button = buttons[i]; i++) {
   mdc.ripple.MDCRipple.attachTo(button);
 }
