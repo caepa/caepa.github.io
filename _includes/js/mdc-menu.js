e0bccbf9fc7980cf611c588aca30fe0d6992234c
Jason Kilpatrick <champa720@gmail.com>
2018-10-20 16:23:40 -0600

    Update: v0.40.1

--- _includes/js/mdc-menu.js
+++ _includes/js/mdc-menu.js
@@ -1,20 +1,11 @@
-// Instantiation
-  var menuEl = document.querySelector('#apps-menu');
-  var menu = new mdc.menu.MDCMenu(menuEl);
-  var menuButtonEl = document.querySelector('#apps');
+function initMenu(el) {
 
-  // Toggle menu open
+  var menu = window.dynamicMenu = new mdc.menu.MDCMenu(document.querySelector('#' + el + '-menu'));
+  var menuButtonEl = document.querySelector('#' + el);
+
   menuButtonEl.addEventListener('click', function() {
     menu.open = !menu.open;
   });
+}
 
-  // Listen for selected item
-  menuEl.addEventListener('MDCMenu:selected', function(evt) {
-     var detail = evt.detail;
-  });
-
-  // Set Anchor Corner to Bottom End
-  //menu.setAnchorCorner(Corner.BOTTOM_END);
-
-  // Turn off menu open animations
-  //menu.quickOpen = true;
+initMenu('apps');
